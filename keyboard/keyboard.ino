cd45439053eeac410c3c7c12ff21835c66d2647e
Henrik Sandklef <hesa@sandklef.com>
2019-08-21 12:18:02 +0200

    hw fixes, renames...

--- keyboard/keyboard.ino
+++ keyboard/keyboard.ino
@@ -1,45 +1,108 @@
+/*
+ *
+ *  keyboard.ino - use button to send keys via USB
+ *
+ *  This code is written with some pedagogical concerns in mind.
+ *  The code is not in any way written to be optimal or flexible.
+ *  A struct and an array would, as two examples, make the code way better.
+ *  
+ *  (c) Henrik Sandklef and Rikard Fröberg (juneday.se) 2019
+ * 
+ *  License: none, consider this code to be under Public Domain
+ * 
+ */
 #include "Keyboard.h"
 
-int pin_L = 8;
-int pin_P = 9;
-int pin_S = 10;
-int pin_BackSpace = 11;
-int pin_Enter = 12;
 
-int led_L = 3;
-int led_P = 4;
-int led_S = 5;
-int led_BackSpace = 6;
-int led_Enter = 7;
+// Variables for our leds
+int led_L = 4;
+int led_P = 5;
+int led_S = 6;
+int led_BackSpace = 7;
+int led_Enter = 8;
 
+// Variables for our button
+int button_L = 9;
+int button_P = 10;
+int button_S = 11;
+int button_BackSpace = 12;
+int button_Enter = 13;
+
+
+/**********************************************************
+ * 
+ * void setup()
+ * 
+ * Sets up the hardware 
+ * 
+ ***********************************************************/
 void setup() {
-  pinMode(pin_L, INPUT);
-  pinMode(pin_P, INPUT);
-  pinMode(pin_S, INPUT);
-  pinMode(pin_BackSpace, INPUT);
-  pinMode(pin_Enter, INPUT);
+  // Set the mode of our BUtton pins to INPUT
+  pinMode(button_L, INPUT);
+  pinMode(button_P, INPUT);
+  pinMode(button_S, INPUT);
+  pinMode(button_BackSpace, INPUT);
+  pinMode(button_Enter, INPUT);
+
+  // Set the mode of our Led pins to OUTPUT
   pinMode(led_L, OUTPUT);
   pinMode(led_P, OUTPUT);
   pinMode(led_S, OUTPUT);
   pinMode(led_BackSpace, OUTPUT);
   pinMode(led_Enter, OUTPUT);
+
+  // Setup keyboard 
   Keyboard.begin();
-  Serial.begin(9600);
+
+  // Setup Serial 
+  //  Serial.begin(9600);
 }
 
+
+/**********************************************************
+ * 
+ * int read_button(int button)
+ * 
+ * parameters:
+ *   button  - the pin to read the value from
+ * 
+ * return:
+ *   int - 1 if button is pressed, 0 otherwise
+ * 
+ ***********************************************************/
 int read_button(int button) {
+
+  // Set pin to high (before read)
   digitalWrite(button, HIGH);
+
+  // read and return the reversed value
   return !digitalRead(button);
 }
 
-void handle_pin(int button, int led, int character) {
- /* Serial.print(character);
+/***********************************************************
+ *
+ * void handle_button(int button, int led, int character) 
+ * 
+ * parameters:
+ *   button  - the pin to read the value from
+ *   led     - the led to lit if button is pressed
+ *   character - the character to send to the computer connected via USB
+ * 
+ ***********************************************************/
+void handle_button(int button, int led, int character) {
+ /*
+  Serial.print(character);
   Serial.print(" ");
   Serial.print(button);
   Serial.print(" => ");
   Serial.println(read_button(button));
   */
+
+  // Set led to LOW to not fuke things up when reading button
+  digitalWrite(led,LOW); 
+
   if ( read_button(button) ) {
+    // if the user has pressed the button, send the character
     Keyboard.write(character);    
     digitalWrite(led,HIGH);
   } else {
@@ -48,11 +111,22 @@ void handle_pin(int button, int led, int character) {
   
 }
 
+/***********************************************************
+ *  loop()
+ *  
+ *  After setup this function is called repeatedly
+ *  
+ ***********************************************************/
 void loop() {
-  handle_pin(pin_L, led_L, 'l');  
-  handle_pin(pin_P, led_P, 'p');  
-  handle_pin(pin_S, led_S, 's');  
-  handle_pin(pin_BackSpace, led_BackSpace, '\b');  
-  handle_pin(pin_Enter, led_Enter, '\n');  
+
+  // Handle the buttons
+  handle_button(button_L, led_L, 'l');  
+  handle_button(button_P, led_P, 'p');  
+  handle_button(button_S, led_S, 's');  
+  handle_button(button_BackSpace, led_BackSpace, '\b');  
+  handle_button(button_Enter, led_Enter, '\n'); 
+
+  // 3 repeats per second is ok, so sleep 300 ms
+  // Probably an ok balance between responsiveness and repeat
   delay(300);
 }
